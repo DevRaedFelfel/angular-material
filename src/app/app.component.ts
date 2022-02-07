48284fe09ec54d024d9ea949056a6ef5d619e0d1
Raed Felfel <raedfelfel@outlook.com>
2022-02-07 12:01:17 +0200

    Complete

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements AfterViewInit   {
+  title = 'angular-responsive-sidebar';
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+ 
+   constructor(private observer: BreakpointObserver) {}
+ 
+   ngAfterViewInit() {
+    setTimeout(() => {
+      this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+    }, 1);
+   }
+}
